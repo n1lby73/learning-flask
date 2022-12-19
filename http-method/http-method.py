8408a7960ac5ef89351f22c845a0fa95b06b9c64
n1lby73 <n1lby73@gmail.com>
2022-12-19 22:28:06 +0100

    trying to fix function object is not iterable

--- http-method/http-method.py
+++ http-method/http-method.py
@@ -1,21 +1,20 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, request, url_for
 app = Flask(__name__)
 
-@app.route('/welcome/<name>')
-def welcome(name):
-    return "welcome %s" %name
+@app.route('/post')
+def welcomePage():
+    return "You have succesfully connected to the internet"
 
-# @app.route('/get')
-# def declinePage():
-#     return "Access denied"
+@app.route('/get')
+def declinePage():
+    return "Access denied"
 
 @app.route('/login', methods=['POST','GET'])
 def login():
     if request.method == 'POST':
-        user = request.form("username")
-        return redirect(url_for(welcome))
-    # else:
-    #     return redirect(url_for(declinePage))
+        return redirect(url_for(welcomePage))
+    else:
+        return redirect(url_for(declinePage))
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0',port=3565)
