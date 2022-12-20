08a9b94fbee241537382f8d1b5a78f82aedcedb1
n1lby73 <n1lby73@gmail.com>
2022-12-20 14:30:22 +0100

    Learning templates

---
+++ templates.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def temp():
+    return render_template('netflixlandingpage/index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=3565)
