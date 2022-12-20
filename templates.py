d27ed71a2a80367081b2db9ad7b767b09c30280f
n1lby73 <n1lby73@gmail.com>
2022-12-20 22:06:25 +0100

    learning static files in flask

--- templates.py
+++ templates.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def temp():
-    return render_template('netflixlandingpage/index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3565)
