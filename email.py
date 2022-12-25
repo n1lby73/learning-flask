39291259b2bf2719fc9a35ab2de164178914b133
n1lby73 <n1lby73@gmail.com>
2022-12-25 10:47:29 +0100

    pushed untested email.py file due to power issue

---
+++ email.py
@@ -0,0 +1,26 @@
+from flask import *
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_DEBUG'] = True
+app.config['MAIL_USERNAME'] = 'learningflaskemail'
+app.config['MAIL_PASSWORD'] = '3j6wYW2cjwsWiQX'
+app.config['MAIL_DEFAULT_SENDER'] = ('practice email sending', 'learningflaskemail@gmail.com')
+
+@app.route('/')
+def index():
+    return '<a href="email">click here to recieve an email</a>'
+
+@app.route('/email')
+def email():
+    msg = Message('Testing flask mail framework', recipients=['fwmyhdglyjabcgexuo@tmmwj.net'])
+    msg.body = 'did it work'
+
+    Mail.send(msg)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=3565)
