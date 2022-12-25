6e87aab0be4028a80685df80e4dc36fee8933879
n1lby73 <n1lby73@gmail.com>
2022-12-25 22:27:00 +0100

    turned mail_use_tls value to false

--- flaskEmail.py
+++ flaskEmail.py
@@ -4,7 +4,7 @@ from flask_mail import Mail, Message
 app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_DEBUG'] = True
 app.config['MAIL_USERNAME'] = 'learningflaskemail@gmail.com'
