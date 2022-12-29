d7b28682caee35789056489851eb1ecf705d6579
n1lby73 <n1lby73@gmail.com>
2022-12-29 08:20:26 +0100

    debugging validators

--- flaskForm.py
+++ flaskForm.py
@@ -4,9 +4,11 @@ from form import regForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] ="trying to work with wtforms"
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def index():
     form=regForm()
+    if form.validate_on_submit():
+        return "few"
     return render_template('form-index.html',form=form)
 
 if __name__ == '__main__':
--- form.py
+++ form.py
@@ -6,10 +6,10 @@ class regForm (FlaskForm):
 
     username = StringField('username', validators=[InputRequired(), Length(min=5, max=20, message='username must be between 5-20 characters')])
 
-    email = EmailField('email', validators=[InputRequired(), Email('Input a valid email')])
+    email = EmailField('email', validators=[InputRequired(), Email('Input a valid email')]) 
 
     password = PasswordField("password", validators=[InputRequired(), Length(min=8, max=20, message='password must be  between 8-20 characters')])
 
-    confirmpass = PasswordField("Confirm password", validators=[InputRequired(),EqualTo('password')])
+    confirmpass = PasswordField("Confirm password", validators=[InputRequired(), EqualTo('password')])
 
     reg = SubmitField("login")
