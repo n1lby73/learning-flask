5a8b4b201c830547b9e69d422ec205d17b2d2fb7
n1lby73 <n1lby73@gmail.com>
2022-12-29 01:19:46 +0100

    debugging validators

--- form.py
+++ form.py
@@ -4,12 +4,12 @@ from wtforms.validators import InputRequired, Length, Email, EqualTo
 
 class regForm (FlaskForm):
 
-    username = StringField('username')
+    username = StringField('username', validators=[InputRequired(), Length(min=5, max=20, message='username must be between 5-20 characters')])
 
-    email = EmailField('email')
+    email = EmailField('email', validators=[InputRequired(), Email('Input a valid email')])
 
-    password = PasswordField("password")
+    password = PasswordField("password", validators=[InputRequired(), Length(min=8, max=20, message='password must be  between 8-20 characters')])
 
-    confirmpass = PasswordField("Confirm password")
+    confirmpass = PasswordField("Confirm password", validators=[InputRequired(),EqualTo('password')])
 
     reg = SubmitField("login")
