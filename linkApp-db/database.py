ce988b55d50cc7415bce21fcc8e9a7e5b29ddd10
n1lby73 <n1lby73@gmail.com>
2023-01-14 00:42:11 +0100

    debugged html form and added some routes

--- linkApp-db/database.py
+++ linkApp-db/database.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    return "#"
+    if request.method == 'POST':
+        username = request.form['username']
+        return f'hello {username}'
+    else:
+        return redirect(url_for('index'))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=3565)
