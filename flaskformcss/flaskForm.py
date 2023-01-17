e9af2f975f320d8a54617619c067748606822d63
n1lby73 <n1lby73@gmail.com>
2023-01-17 07:28:53 +0100

    new branch to add css styling to flask form

---
+++ flaskformcss/flaskForm.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from form import regForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] ="trying to work with wtforms"
+
+@app.route('/', methods=['POST','GET'])
+def index():
+    form=regForm()
+    if form.validate_on_submit():
+        return "account created successfully"
+    return render_template('form-index.html',form=form)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=3565)
---
+++ flaskformcss/form.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, Email, EqualTo
+
+class regForm (FlaskForm):
+    
+    username = StringField('username', validators=[InputRequired(), Length(min=5, max=20, message='username must be between 5-20 characters')])
+
+    email = EmailField('email', validators=[InputRequired(), Email('Input a valid email')]) 
+
+    password = PasswordField("password", validators=[InputRequired(), EqualTo("confirmpass",message="password must be the same"), Length(min=8, max=20, message='password must be  between 8-20 characters')])
+
+    confirmpass = PasswordField("Confirm password", validators=[InputRequired(), EqualTo('password', message="passwords must be same")])
+
+    reg = SubmitField("sign up")
