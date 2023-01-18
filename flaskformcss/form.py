b62eaaa73f43945fc342624ffa82a7ca1f6a8121
n1lby73 <n1lby73@gmail.com>
2023-01-18 18:01:08 +0100

    useing flask form for validation instead of js and working on styling

--- flaskformcss/form.py
+++ flaskformcss/form.py
@@ -4,12 +4,12 @@ from wtforms.validators import InputRequired, Length, Email, EqualTo
 
 class regForm (FlaskForm):
     
-    username = StringField('username', validators=[InputRequired(), Length(min=5, max=20, message='username must be between 5-20 characters')])
+    username = StringField('Username', validators=[InputRequired(), Length(min=5, max=20, message='username must be between 5-20 characters')])
 
-    email = EmailField('email', validators=[InputRequired(), Email('Input a valid email')]) 
+    email = EmailField('Email', validators=[InputRequired(), Email('Input a valid email')]) 
 
-    password = PasswordField("password", validators=[InputRequired(), EqualTo("confirmpass",message="password must be the same"), Length(min=8, max=20, message='password must be  between 8-20 characters')])
+    password = PasswordField("Password", validators=[InputRequired(), EqualTo("confirmpass",message="password must be the same"), Length(min=8, max=20, message='password must be  between 8-20 characters')])
 
     confirmpass = PasswordField("Confirm password", validators=[InputRequired(), EqualTo('password', message="passwords must be same")])
 
-    reg = SubmitField("sign up")
+    reg = SubmitField("Sign up")
