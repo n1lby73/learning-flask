680ef5d40bc5b8cfdbbc05813677bcf926952154
n1lby73 <n1lby73@gmail.com>
2023-01-25 14:51:52 +0100

    made a  typo in the success page to make a new commit

--- validatingWithJs/database.py
+++ validatingWithJs/database.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/success')
+@app.route('/succes')
 def success():
     return render_template('success.html')
 
