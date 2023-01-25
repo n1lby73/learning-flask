58f4290a5cb7279106f6444cd369772259ce1779
n1lby73 <n1lby73@gmail.com>
2023-01-25 14:53:13 +0100

    fixed the typo and added a error 404 handler route

--- validatingWithJs/database.py
+++ validatingWithJs/database.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/succes')
+@app.route('/success')
 def success():
     return render_template('success.html')
 
