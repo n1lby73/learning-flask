04cbce471f2ba140a88967a60a8b0791b2caadfc
n1lby73 <n1lby73@gmail.com>
2023-01-25 14:50:30 +0100

    added success route and returned jsonfy value to js

--- validatingWithJs/database.py
+++ validatingWithJs/database.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, jsonify
 
 app = Flask(__name__)
 
@@ -12,16 +12,21 @@ def success():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    if request.method == 'POST':
-        data = request.get_json()
-
-        username = data["username"]
-        email = data["email"]
-        password = data["password"]
-
-        return "post request"
-    else:
+    if request.method != 'POST':
         return redirect(url_for('index'))
-        
+    
+    data = request.get_json()
+
+    username = data["username"]
+    email = data["email"]
+    password = data["password"]
+
+    return jsonify(success=True)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=3565)
