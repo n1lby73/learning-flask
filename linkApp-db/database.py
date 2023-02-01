146ad3ca58f3e86b64049c7568423298c9d6dc37
n1lby73 <n1lby73@gmail.com>
2023-02-01 07:31:18 +0100

    created table, added error page handler and commented out app.app_context().push() to test run the program without it

--- linkApp-db/database.py
+++ linkApp-db/database.py
@@ -1,6 +1,26 @@
+import os
 from flask import Flask, render_template, url_for, request, redirect, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
+app.app_context().push()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+
+db = SQLAlchemy(app)
+
+class linkAppDb(db.Model):
+
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), nullable = False)
+    email = db.Column(db.String(50), nullable = False)
+    password = db.Column(db.String(50), nullable = False)
+
+    def __repr__(self):
+        return (f"linkAppDb('{self.username}', '{self.email}')")
 
 @app.route('/')
 def index():
@@ -21,6 +41,11 @@ def login():
     email = data["email"]
     password = data["password"]
 
+    new_user = linkAppDb(username=username, email=email, password=password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
     return jsonify(success=True)
 
 @app.errorhandler(404)
@@ -29,4 +54,5 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(host='0.0.0.0', debug=True, port=3566)
